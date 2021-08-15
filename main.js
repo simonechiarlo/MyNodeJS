23723666b52c9f666761f9a4f26f5916d8453f7e
simonechiarlo <simonechiarlo1@gmail.com>
2021-08-15 15:35:19 +0200

    minchiatanta

--- main.js
+++ main.js
@@ -1,34 +1,11 @@
-const http = require("http");
-var url = require("url");
-const hostname = '127.0.0.1';
-const port = 8081;
-var a=0;
-setInterval(generaVal,5000);
+const app = require("express")();
 
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-}
+const PORT = process.env.PORT || 3000;
 
-function generaVal(){
-    a = getRandomInt(10)+1;
-}
-
-
-
-
-
-const server = http.createServer((req, res) => {
-    if(req.url === "/test"){
-        res.write(a.toString());
-    }
-    /*
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  */
-  res.end();
+app.get("", (req, res) => {
+  res.send("Hello world");
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server BINGO running at http://${hostname}:${port}/test`);
+app.listen(PORT, () => {
+  console.log(`App up at port ${PORT}`);
 });
-
