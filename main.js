d52d6b075e69a4f0561709b85fd70e0eb65bc32b
simonechiarlo <simonechiarlo1@gmail.com>
2021-08-15 15:40:01 +0200

    bingo

--- main.js
+++ main.js
@@ -2,8 +2,22 @@ const app = require("express")();
 
 const PORT = process.env.PORT || 3000;
 
+var a=0;
+setInterval(generaVal,5000);
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function generaVal(){
+    a = getRandomInt(10)+1;
+}
+
+
+
 app.get("", (req, res) => {
-  res.send("Hello world");
+  res.write(a.toString());
+  res.end();
 });
 
 app.listen(PORT, () => {
