bd837f04ed215f88f55581453044c04652fcf9c5
simonechiarlo <simonechiarlo1@gmail.com>
2021-08-15 14:40:10 +0200

    first commit

---
+++ check.py
@@ -0,0 +1,7 @@
+import os
+from time import sleep
+while True:
+    os.system('cmd /c  "curl localhost:8081/pippe && echo("\n")"')
+    os.system('cmd /c  ""')
+    
+
---
+++ main.js
@@ -0,0 +1,34 @@
+const http = require("http");
+var url = require("url");
+const hostname = '127.0.0.1';
+const port = 8081;
+var a=0;
+setInterval(generaVal,5000);
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function generaVal(){
+    a = getRandomInt(10)+1;
+}
+
+
+
+
+
+const server = http.createServer((req, res) => {
+    if(req.url === "/pippe"){
+        res.write(a.toString());
+    }
+    /*
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  */
+  res.end();
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/pippe`);
+});
+
