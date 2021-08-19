81d5462652c14fed239ba1e45d97425bfb6414a2
simonechiarlo <simonechiarlo1@gmail.com>
2021-08-19 09:57:17 +0200

    lasterrima

--- check.py
+++ check.py
@@ -1,7 +1,7 @@
 import os
 from time import sleep
 while True:
-    os.system('cmd /c  "curl https://nodejskey.herokuapp.com/ && echo("\n")"')
+    os.system('cmd /c  "curl https://nodejskey.herokuapp.com/ && echo("\n")"') #in pratica curl e echo sono normali comandi del cmd, chiamati da python
     os.system('cmd /c  ""')
     
 
--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const app = require("express")();
 const PORT = process.env.PORT || 3000;
 
 var a=0;
-setInterval(generaVal,5000);
+setInterval(generaVal,5000); //chiamo automaticamente questa funzione ogni 5 secondi
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
@@ -11,6 +11,7 @@ function getRandomInt(max) {
 
 function generaVal(){
     a = getRandomInt(10)+1;
+    //ci saranno poi altre cose da fare
 }
 
 
