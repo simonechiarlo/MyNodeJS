d5def8bed9efdd10b0f87c122a48763f52b57ad9
simonechiarlo <simonechiarlo1@gmail.com>
2021-08-19 09:47:45 +0200

    last

--- check.py
+++ check.py
@@ -1,7 +1,7 @@
 import os
 from time import sleep
 while True:
-    os.system('cmd /c  "curl localhost:8081/test && echo("\n")"')
+    os.system('cmd /c  "curl https://nodejskey.herokuapp.com/ && echo("\n")"')
     os.system('cmd /c  ""')
     
 
